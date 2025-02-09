5701680daa5ab2471a64d60b76290d1b6ea0f969
Art3mis15 <awesomeness.siew@gmail.com>
2025-02-09 05:29:17 -0800

    preload stuff

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -3,9 +3,28 @@ class Menu extends Phaser.Scene {
         super("menuScene")
     }
 
-    create() {
-        this.add.text(20, 20, "menu scene for squash hop")
+    preload() {
+        // load sprites and tiles
+        this.load.image('background', './assets/background.png')
+        this.load.image('platform', './assets/platform.png')
+        
+        // load spritesheet 
+        this.load.spritesheet('blob', './assets/blob.png', {
+            frameWidth: 64,
+            frameHeight: 64,
+        })
 
+        // load audio
+        this.load.audio('bgm', './assets/Variations on an Original.wav')
+        this.load.audio('jump', './assets/jump.wav')
+        this.load.audio('land', './assets/land.wav')
+        this.load.audio('select', './assets/select.wav')
+        this.load.audio('squish', './assets/squish.wav')
+
+    }
+
+    create() {
+        
         // skip to play scene for debugging
         this.scene.start("playScene")
     }
