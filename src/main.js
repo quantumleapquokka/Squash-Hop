342b282c2d0c8fdbe96a84f072d4f621835c9777
Art3mis15 <awesomeness.siew@gmail.com>
2025-02-09 05:16:23 -0800

    main and assets

--- src/main.js
+++ src/main.js
@@ -1,3 +1,11 @@
+// Game Title: Squash Hop
+// Name: Samantha Siew
+// Hours Spent:
+/*Creative Tilt:
+
+*/
+'use strict'
+
 let config = {
     type: Phaser.AUTO,
     width: 800,
@@ -5,3 +13,10 @@ let config = {
     scene: [ Menu, Play, Credits ]
 }
 let game = new Phaser.Game(config)
+
+// Key bindings
+let keySPACE
+
+// UI sizes
+let borderUISize = game.config.height / 15
+let borderPadding = borderUISize / 3
