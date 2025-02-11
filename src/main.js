0911d0106578cff45c4511dab3a810d01681e2d9
Samantha Siew <sammie.siew@gmail.com>
2025-02-11 05:39:19 -0800

    turned off debug

--- src/main.js
+++ src/main.js
@@ -19,7 +19,7 @@ let config = {
     physics: {
         default: 'arcade',
         arcade: {
-            debug: true
+            debug: false
         }
     },
     scene: [ Menu, Play, Credits ]
